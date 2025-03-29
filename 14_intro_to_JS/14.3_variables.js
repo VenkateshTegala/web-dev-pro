630336e89046d7085e542a35672977425a0acbc7
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-29 22:35:20 +0530

    Added new file

---
+++ 14_intro_to_JS/14.3_variables.js
@@ -0,0 +1,25 @@
+//variables
+let x = 10;
+alert(x);
+
+var name = prompt("What is your name ?");
+var myName = "venky";
+alert(myName);
+alert(name);
+alert("My name is " + myName + name + " Welcome !");
+
+var game_level = 1;
+game_level = 2;
+game_level = 3;
+alert("Game level is : " + game_level)
+
+//variable swapping
+var a = "3";
+var b = "8";
+
+var temp = a;
+a = b;
+b = temp;
+console.log("Value of A : " + a);
+console.log("Value of B : " + b);
+
