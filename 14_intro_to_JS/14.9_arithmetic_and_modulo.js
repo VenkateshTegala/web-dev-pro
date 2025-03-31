b54d2fd8b1d45463c727c4d34975c7a296afa2ad
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-31 21:56:46 +0530

    Added new file

---
+++ 14_intro_to_JS/14.9_arithmetic_and_modulo.js
@@ -0,0 +1,10 @@
+console.log(6/4);
+console.log(6 + 4);
+console.log(3 + 5 * 2)
+
+//Dog age to Human age
+//humanAge = (dogAge - 2) x 4 + 21
+
+var dogAge = prompt("Enter dog age : ")
+var humanAge = ((dogAge - 2) * 4) + 21
+console.log("Human age is : " + humanAge);
