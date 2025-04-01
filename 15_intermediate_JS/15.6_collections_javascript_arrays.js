a831505c619db8881b51e2855c4f6a3fb1f88a7a
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-04-01 19:30:16 +0530

    Added new file

---
+++ 15_intermediate_JS/15.6_collections_javascript_arrays.js
@@ -0,0 +1,16 @@
+var eggs = [1, 2, 3, 4, 5];
+console.log(eggs[1]);
+
+//length
+console.log(eggs.length);
+
+var guests = ["Angela", "Bob", "Cherry", "David"];
+console.log(guests);
+
+//checking if includes
+var name = prompt("Enter the name to check :");
+if (guests.includes(name)) {
+    console.log("Yes your name is there");
+} else {
+    console.log("Sorry")
+}
